62cb6bc5298aebda18181e4dbd16480a9d2814d4
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-09-26 18:29:33 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Example usage:
+  const mickey = new Mouse("Mickey", "male");
+  const minnie = new Mouse("Minnie", "female");
+  
+  console.log(mickey.speak()); // "It's me! Mickey, the parrot!"
+  console.log(minnie.speak()); // "Minnie says squawk!"
+  
